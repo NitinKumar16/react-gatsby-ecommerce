472d720818237fcc50b390eaab47c4111189d9d9
NitinKumar16 <nitin16kumar97@gmail.com>
2020-05-07 06:45:55 +0530

    config changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-  path:`.env.development`,
+  path:`.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
@@ -38,20 +38,16 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    module.exports = {
-      plugins: [
         {
           resolve: 'gatsby-plugin-snipcart',
           options: {
             apiKey: process.env.SNIPCART_KEY,
-            autopop: false,
+            autopop: true,
           }
         }
-      ]
+      ],
     }
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
